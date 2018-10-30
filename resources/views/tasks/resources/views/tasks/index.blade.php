5a24fec703fa392778fcdbf9771a0bf5f2374c05
mayuko.honda <hiro.saya.kouchan@gmail.com>
2018-10-30 11:13:23 +0000

    CRUD tasks

---
+++ resources/views/tasks/resources/views/tasks/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>メッセージ一覧</h1>
+
+    @if (count($tasks) > 0)
+        <ul>
+            @foreach ($tasks as $task)
+                <li>{{ $task->content }}</li>
+            @endforeach
+        </ul>
+    @endif
+
+
+@endsection
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -93,3 +93,13 @@
         </div>
     </body>
 </html>
+
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>id = {{ $task->id }} のメッセージ詳細ページ</h1>
+
+    <p>{{ $task->content }}</p>
+
+@endsection
