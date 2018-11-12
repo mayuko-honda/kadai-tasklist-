f67e7efafe2e5e2ee793f81a5b767fab889e218a
mayuko.honda <hiro.saya.kouchan@gmail.com>
2018-11-12 04:53:11 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,7 +5,12 @@
     <h1>タスク新規作成ページ</h1>
     
     <div class="row">
-        <div class="col-xs-6">
+        <div class="col-xs-12">
+     　　<div class="col-sm-5">col-sm-5</div>
+        <div class="col-sm-offset-2 col-sm-5">col-sm-5</div>
+        <div class="col-md-5">col-md-5</div>
+        <div class="col-md-offset-2 col-md-5">col-md-5</div>
+        <div class="col-lg-offset-3 ">col-lg-3</div>
 
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
     
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -2,7 +2,12 @@
 
 @section('content')
    <div class="row">
-        <div class="col-xs-6">
+            <div class="col-xs-12">
+     　　<div class="col-sm-3">col-sm-3</div>
+        <div class="col-sm-offset-2 col-sm-5">col-sm-5</div>
+        <div class="col-md-5">col-md-5</div>
+        <div class="col-md-offset-2 col-md-5">col-md-5</div>
+        <div class="col-lg-offset-3 ">col-lg-3</div>
 
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
     
